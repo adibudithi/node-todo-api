5aa614f64eb458cef30970557333f9a293886593
adibudithi <adibudithi@gmail.com>
2018-06-27 21:26:03 -0700

    added db and models

---
+++ server/models/todos.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+module.exports = {
+  Todo
+};
